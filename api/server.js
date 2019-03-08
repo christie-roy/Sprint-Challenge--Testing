c5e8f7ef2a5b0e0f15f383b333f8fc833c271656
croy <roy.christie95@gmail.com>
2019-03-08 10:35:49 -0600

    Added route handler to initial route

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const games = require('');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    res.status(200).json({ msg: 'Running!'});
+});
